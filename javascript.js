8ee3d196c2f3aad3bdc07169ad3c760417060f36
leslied41 <leslied41@gmail.com>
2021-10-03 14:44:22 +1100

    replace work ok now

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-const robotEl = document.getElementById("robot");
-const robotImage = document.getElementById("robotImg");
 const form = document.getElementById("form");
 const xEl = document.getElementById("x");
 const yEl = document.getElementById("y");
@@ -8,24 +6,69 @@ const rightBtn = document.getElementById("right");
 const leftBtn = document.getElementById("left");
 const moveBtn = document.getElementById("move");
 const reportBtn = document.getElementById("report");
+const robotImgCollection = document.getElementsByTagName("img");
+const gridEl = document.getElementById("grid");
 
+let robotList = [];
 let x = 0;
 let y = 0;
 
-window.addEventListener("load", () => {
-  robotEl.style.left = 0;
-  robotEl.style.bottom = 0;
-});
+// newRobotBtn.addEventListener("click", () => {
+//   const robotId = new Date().getTime().toString();
+
+//   robotList.push(robotId);
+//   console.log(robotList);
+
+//   robotsEl.innerHTML = `
+//         ${robotList.map(
+//           (id) => `<div class='robots' id=${id}><img
+//             class="robot_spritesheet pixelart"
+//             id='${id}'
+//             src="./robot.png"
+//             alt="robot"
+//           /></div>`
+//         )}
+
+//   `;
+
+//   containerEl.replaceChild(robotsEl, robotsEl);
+
+//   // robotsEl.insertAdjacentHTML(
+//   //   "afterbegin",
+//   //   `
+//   //       ${robotList.map(
+//   //         (id) => `<div class='robots' id=${id}><img
+//   //           class="robot_spritesheet pixelart"
+//   //           id='${id}'
+//   //           src="./robot.png"
+//   //           alt="robot"
+//   //         /></div>`
+//   //       )}
+
+//   // `
+//   // );
+// });
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  const robotEl = document.createElement("div");
 
+  robotEl.innerHTML = ` <img
+            class="robot_spritesheet pixelart"
+            id="robotImg"
+            src="./robot.png"
+            alt="robot"
+          />`;
+  robotEl.classList.add("robot");
+  robotEl.setAttribute("id", "robot");
+  gridEl.appendChild(robotEl);
+  const robotImage = document.getElementById("robotImg");
   const xValue = xEl.value;
   const yValue = yEl.value;
   const fValue = fEl.value;
   if (xValue && yValue && fValue) {
-    x = xValue;
-    y = yValue;
+    x = parseInt(xValue);
+    y = parseInt(yValue);
     robotEl.style.left = `${xValue}px`;
     robotEl.style.bottom = `${yValue}px`;
     switch (fValue.toUpperCase()) {
@@ -46,81 +89,70 @@ form.addEventListener("submit", (e) => {
         break;
     }
   }
-});
-
-window.addEventListener("keyup", (e) => {
-  if (e.key === "ArrowLeft") {
-    robotImage.classList.value = "robot_spritesheet pixelart face-left";
-  }
-  if (e.key === "ArrowRight") {
-    robotImage.classList.value = "robot_spritesheet pixelart face-right";
-  }
-  if (e.key === "ArrowUp") {
-    robotImage.classList.value = "robot_spritesheet pixelart face-back";
-  }
-  if (e.key === "ArrowDown") {
-    robotImage.classList.value = "robot_spritesheet pixelart ";
-  }
-});
-
-rightBtn.addEventListener("click", () => {
-  if (robotImage.classList.contains("face-back")) {
-    robotImage.classList.value = "robot_spritesheet pixelart face-right";
-  } else if (robotImage.classList.contains("face-left")) {
-    robotImage.classList.value = "robot_spritesheet pixelart face-back";
-  } else if (robotImage.classList.contains("face-right")) {
-    robotImage.classList.value = "robot_spritesheet pixelart";
-  } else {
-    robotImage.classList.value = "robot_spritesheet pixelart face-left";
-  }
-});
-
-leftBtn.addEventListener("click", () => {
-  if (robotImage.classList.contains("face-back")) {
-    robotImage.classList.value = "robot_spritesheet pixelart face-left";
-  } else if (robotImage.classList.contains("face-left")) {
-    robotImage.classList.value = "robot_spritesheet pixelart ";
-  } else if (robotImage.classList.contains("face-right")) {
-    robotImage.classList.value = "robot_spritesheet pixelart face-back";
-  } else {
-    robotImage.classList.value = "robot_spritesheet pixelart face-right";
-  }
-});
-
-moveBtn.addEventListener("click", () => {
-  if (robotImage.classList.contains("face-back")) {
-    if (y >= 0 && y <= 300) {
-      y = y + 100;
-      robotEl.style.bottom = `${y}px`;
+  //turn right
+  const turnRight = () => {
+    if (robotImage.classList.contains("face-back")) {
+      robotImage.classList.value = "robot_spritesheet pixelart face-right";
+    } else if (robotImage.classList.contains("face-left")) {
+      robotImage.classList.value = "robot_spritesheet pixelart face-back";
+    } else if (robotImage.classList.contains("face-right")) {
+      robotImage.classList.value = "robot_spritesheet pixelart";
+    } else {
+      robotImage.classList.value = "robot_spritesheet pixelart face-left";
     }
-  } else if (robotImage.classList.contains("face-left")) {
-    if (x > 0 && x <= 400) {
-      x = x - 100;
-      robotEl.style.left = `${x}px`;
-    }
-  } else if (robotImage.classList.contains("face-right")) {
-    if (x >= 0 && x <= 300) {
-      x = x + 100;
-      robotEl.style.left = `${x}px`;
-    }
-  } else {
-    if (y > 0 && y <= 400) {
-      y = y - 100;
-      robotEl.style.bottom = `${y}px`;
-    }
-  }
-});
+  };
+  rightBtn.addEventListener("click", turnRight);
 
-reportBtn.addEventListener("click", () => {
-  let facing_direction;
-  if (robotImage.classList.contains("face-back")) {
-    facing_direction = "NORTH";
-  } else if (robotImage.classList.contains("face-left")) {
-    facing_direction = "WEST";
-  } else if (robotImage.classList.contains("face-right")) {
-    facing_direction = "EAST";
-  } else {
-    facing_direction = "SOUTH";
-  }
-  alert(`X: ${x / 100}. Y: ${y / 100}. Facing direction: ${facing_direction}.`);
+  //turn left
+  leftBtn.addEventListener("click", () => {
+    if (robotImage.classList.contains("face-back")) {
+      robotImage.classList.value = "robot_spritesheet pixelart face-left";
+    } else if (robotImage.classList.contains("face-left")) {
+      robotImage.classList.value = "robot_spritesheet pixelart ";
+    } else if (robotImage.classList.contains("face-right")) {
+      robotImage.classList.value = "robot_spritesheet pixelart face-back";
+    } else {
+      robotImage.classList.value = "robot_spritesheet pixelart face-right";
+    }
+  });
+  //move
+  moveBtn.addEventListener("click", () => {
+    if (robotImage.classList.contains("face-back")) {
+      if (y >= 0 && y <= 300) {
+        y = y + 100;
+        robotEl.style.bottom = `${y}px`;
+      }
+    } else if (robotImage.classList.contains("face-left")) {
+      if (x > 0 && x <= 400) {
+        x = x - 100;
+        robotEl.style.left = `${x}px`;
+      }
+    } else if (robotImage.classList.contains("face-right")) {
+      if (x >= 0 && x <= 300) {
+        x = x + 100;
+        robotEl.style.left = `${x}px`;
+      }
+    } else {
+      if (y > 0 && y <= 400) {
+        y = y - 100;
+        robotEl.style.bottom = `${y}px`;
+      }
+    }
+  });
+  //report
+  reportBtn.addEventListener("click", () => {
+    let facing_direction;
+    if (robotImage.classList.contains("face-back")) {
+      facing_direction = "NORTH";
+    } else if (robotImage.classList.contains("face-left")) {
+      facing_direction = "WEST";
+    } else if (robotImage.classList.contains("face-right")) {
+      facing_direction = "EAST";
+    } else {
+      facing_direction = "SOUTH";
+    }
+    alert(
+      `X: ${x / 100}. Y: ${y / 100}. Facing direction: ${facing_direction}.`
+    );
+  });
 });
